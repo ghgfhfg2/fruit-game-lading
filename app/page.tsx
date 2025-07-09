2177f428f8eac96bdd225e5bf309536947ceb9c6
sooya <ghgfhfg2@naver.com>
2025-07-09 22:45:30 +0900

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import Hero from "./components/Hero";
-import GamePreview from "./components/GamePreview";
 import Features from "./components/Features";
-import Testimonials from "./components/Testimonials";
 import Footer from "./components/Footer";
 
 export default function Home() {
---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://fruit-game-lading.pages.dev",
+  generateRobotsTxt: true,
+};
