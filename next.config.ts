1940ec89940d924cb09572712f363504ec74f896
sooya <ghgfhfg2@naver.com>
2025-07-09 23:14:35 +0900

    .

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/fruit-game-lading",
+  assetPrefix: "/fruit-game-lading",
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    // ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
